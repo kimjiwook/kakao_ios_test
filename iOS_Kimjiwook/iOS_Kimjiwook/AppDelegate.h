2d134dc5c6b3f1556a18a3c51e59fbde8d9de0cb
kimjiwook <xodhks_0113@naver.com>
2018-04-11 13:29:32 +0900

    First Project Create....

---
+++ iOS_Kimjiwook/iOS_Kimjiwook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Kimjiwook
+//
+//  Created by kimjiwook on 2018. 4. 11..
+//  Copyright © 2018년 kimjiwook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Kimjiwook/iOS_Kimjiwook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOS_Kimjiwook
+//
+//  Created by kimjiwook on 2018. 4. 11..
+//  Copyright © 2018년 kimjiwook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
